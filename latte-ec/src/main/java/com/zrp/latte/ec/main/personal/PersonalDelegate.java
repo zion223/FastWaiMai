237a47117187e4a5f2197ea01b6c649d539e0da2
Jayruiping <1025618933@qq.com>
2019-11-22 11:07:01 +0800

    重构优惠券部分提交

--- latte-ec/src/main/java/com/zrp/latte/ec/main/personal/PersonalDelegate.java
+++ latte-ec/src/main/java/com/zrp/latte/ec/main/personal/PersonalDelegate.java
@@ -1,36 +1,30 @@
 package com.zrp.latte.ec.main.personal;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.chad.library.adapter.base.entity.MultiItemEntity;
 import com.example.latte.latte_ec.R;
 import com.example.latte.latte_ec.R2;
 import com.zrp.latte.delegates.bottom.BottomItemDelegate;
 import com.zrp.latte.ec.main.personal.address.AddressDelegate;
 import com.zrp.latte.ec.main.personal.discount.DiscountCardAdapter;
-import com.zrp.latte.ec.main.personal.discount.DiscountCardBean;
-import com.zrp.latte.ec.main.personal.discount.DiscountCardItemType;
+import com.zrp.latte.ec.main.personal.discount.DiscountCardDataConverter;
 import com.zrp.latte.ec.main.personal.order.OrderDelegate;
-import com.zrp.latte.ui.recycler.MultipleItemEntity;
+import com.zrp.latte.net.RestClient;
+import com.zrp.latte.net.callback.ISuccess;
+import com.zrp.latte.ui.recycler.DataConverter;
 
-import java.util.ArrayList;
 
 import butterknife.BindView;
-import butterknife.ButterKnife;
 import butterknife.OnClick;
-import butterknife.Unbinder;
 
 public class PersonalDelegate extends BottomItemDelegate {
 
@@ -74,13 +68,10 @@ public class PersonalDelegate extends BottomItemDelegate {
 
     @Override
     public void onBindView(@Nullable Bundle savedInstanceState, @NonNull View view) {
-        //模拟优惠券数据
-        final ArrayList discountCardData = initDiscardCountList();
 
         final LinearLayoutManager manager = new LinearLayoutManager(getContext());
         mRvDiscountCard.setLayoutManager(manager);
-        final DiscountCardAdapter discountCardAdapter = new DiscountCardAdapter(discountCardData);
-        mRvDiscountCard.setAdapter(discountCardAdapter);
+
         //收货地址
         mTvPersonalAddress.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -91,56 +82,21 @@ public class PersonalDelegate extends BottomItemDelegate {
         });
     }
 
-    @NonNull
-    private ArrayList initDiscardCountList() {
-        //优惠劵卡包数据绑定
-        final DiscountCardBean bean1 = new DiscountCardBean.Builder()
-                .setDelegate(this)
-                .setItemType(DiscountCardItemType.ITEM_NORMAL)
-                .setDiscountCardName("生活卷")
-                .setDiscountCardDetail("让生活更美好")
-                .setDiscountCardUrlId(R.drawable.user_8)
-                .setOnClickListener(new View.OnClickListener(){
+    @Override
+    public void onLazyInitView(@Nullable Bundle savedInstanceState) {
+
+        RestClient.builder()
+                .url("api/discount")
+                .success(new ISuccess() {
                     @Override
-                    public void onClick(View v) {
-                        Toast.makeText(_mActivity, "生活卷", Toast.LENGTH_SHORT).show();
+                    public void onSuccess(String response) {
+                        final DataConverter cardDataConverter = new DiscountCardDataConverter().setJsonData(response);
+                        final DiscountCardAdapter discountCardAdapter = new DiscountCardAdapter(cardDataConverter.convert());
+                        mRvDiscountCard.setAdapter(discountCardAdapter);
                     }
                 })
-                .setId(1)
-                .build();
-        final DiscountCardBean bean2 = new DiscountCardBean.Builder()
-                .setDelegate(this)
-                .setItemType(DiscountCardItemType.ITEM_NORMAL)
-                .setDiscountCardName("网店券")
-                .setDiscountCardDetail("暂无可用优惠券")
-                .setDiscountCardUrlId(R.drawable.user_9)
-                .setOnClickListener(new View.OnClickListener(){
-                    @Override
-                    public void onClick(View v) {
-                        Toast.makeText(_mActivity, "生活卷", Toast.LENGTH_SHORT).show();
-                    }
-                })
-                .setId(2)
-                .build();
-        final DiscountCardBean bean3 = new DiscountCardBean.Builder()
-                .setDelegate(this)
-                .setItemType(DiscountCardItemType.ITEM_NORMAL)
-                .setDiscountCardName("会员卡")
-                .setDiscountCardDetail("一卡在手，逛店无忧")
-                .setDiscountCardUrlId(R.drawable.user_10)
-                .setOnClickListener(new View.OnClickListener(){
-                    @Override
-                    public void onClick(View v) {
-                        Toast.makeText(_mActivity, "生活卷", Toast.LENGTH_SHORT).show();
-                    }
-                })
-                .setId(2)
-                .build();
-        final ArrayList discountCardData = new ArrayList<DiscountCardBean>();
-        discountCardData.add(bean1);
-        discountCardData.add(bean2);
-        discountCardData.add(bean3);
-        return discountCardData;
+                .build()
+                .get();
     }
 
     @OnClick(R2.id.tv_all_order)
