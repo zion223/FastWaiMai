a5fca559ac7fde83c788869f51ed633b747dc200
zion223 <1025618933@qq.com>
2020-08-14 10:32:42 +0800

    refactor: 重构购物券界面网络请求

--- latte-ec/src/main/java/com/zrp/latte/ec/main/personal/p_capital/discount/DiscountViewPageDelegate.java
+++ latte-ec/src/main/java/com/zrp/latte/ec/main/personal/p_capital/discount/DiscountViewPageDelegate.java
@@ -1,5 +1,6 @@
 package com.zrp.latte.ec.main.personal.p_capital.discount;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -11,13 +12,15 @@ import com.example.latte.latte_ec.R;
 import com.example.latte.latte_ec.R2;
 import com.zrp.latte.delegates.bottom.BottomItemDelegate;
 import com.zrp.latte.net.RestClient;
-import com.zrp.latte.net.callback.ISuccess;
 import com.zrp.latte.ui.recycler.DataConverter;
 import com.zrp.latte.ui.recycler.MultipleItemEntity;
 
 import java.util.LinkedList;
 
 import butterknife.BindView;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
 
 public class DiscountViewPageDelegate extends BottomItemDelegate {
 
@@ -29,36 +32,7 @@ public class DiscountViewPageDelegate extends BottomItemDelegate {
 
     private DiscountAdapter mAdapter = null;
 
-    @Override
-    public Object setLayout() {
-        return R.layout.delegate_normal_recyclerview;
-    }
 
-    @Override
-    public void onBindView(@Nullable Bundle savedInstanceState, @NonNull View view) {
-        super.onBindView(savedInstanceState, view);
-        final LinearLayoutManager manager = new LinearLayoutManager(getContext());
-        recyclerView.setLayoutManager(manager);
-        Bundle arguments = getArguments();
-        final int status = arguments.getInt(ARGS_DISCOUNT_STATUS);
-        RestClient.builder()
-                .url("api/p_discount/" + status)
-                .loader(getContext())
-                .success(new ISuccess() {
-                    @Override
-                    public void onSuccess(String response) {
-                        final DataConverter converter = new DiscountDataConverter().setJsonData(response);
-                        LinkedList<MultipleItemEntity> entities = converter.convert();
-                        mAdapter = new DiscountAdapter(entities);
-                        recyclerView.setAdapter(mAdapter);
-                        recyclerView.setBackgroundColor(getResources().getColor(R.color.app_background));
-
-                    }
-                })
-                .build()
-                .get();
-
-    }
     public static DiscountViewPageDelegate create(int orderStatus) {
         final Bundle args = new Bundle();
         args.putInt(ARGS_DISCOUNT_STATUS, orderStatus);
@@ -67,6 +41,39 @@ public class DiscountViewPageDelegate extends BottomItemDelegate {
         return delegate;
     }
 
+    @Override
+    public Object setLayout() {
+        return R.layout.delegate_normal_recyclerview;
+    }
 
+    @SuppressLint("CheckResult")
+    @Override
+    public void onBindView(@Nullable Bundle savedInstanceState, @NonNull View view) {
+        super.onBindView(savedInstanceState, view);
+        final LinearLayoutManager manager = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(manager);
+        Bundle arguments = getArguments();
+        if (arguments != null) {
+            final int status = arguments.getInt(ARGS_DISCOUNT_STATUS);
+            RestClient.builder()
+                    .url("api/p_discount/" + status)
+                    .build()
+                    .get()
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe(new Consumer<String>() {
+                        @Override
+                        public void accept(String response) throws Exception {
+                            final DataConverter converter = new DiscountDataConverter().setJsonData(response);
+                            LinkedList<MultipleItemEntity> entities = converter.convert();
+                            mAdapter = new DiscountAdapter(entities);
+                            recyclerView.setAdapter(mAdapter);
+                            recyclerView.setBackgroundColor(getResources().getColor(R.color.app_background));
+                        }
+                    });
+        }
+
+
+    }
 
 }
